290456136fb7d16694381a75a5ffcba6f7354fe4
Nikunj018 <delvadiyanikunj1234@gmail.com>
2022-02-28 08:40:21 +0530

    Add project files.

---
+++ TempDbFirst/Models/EmployeeMaster.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace TempDbFirst.Models
+{
+    public partial class EmployeeMaster
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeFirstName { get; set; }
+        public string EmployeeLastName { get; set; }
+    }
+}
---
+++ TempDbFirst/Models/TempCRUDdbfirstContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace TempDbFirst.Models
+{
+    public partial class TempCRUDdbfirstContext : DbContext
+    {
+        public TempCRUDdbfirstContext()
+        {
+        }
+
+        public TempCRUDdbfirstContext(DbContextOptions<TempCRUDdbfirstContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<EmployeeMaster> EmployeeMasters { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+/*#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=LAPTOP-6QRR47IT\\SQLEXPRESS;Database=TempCRUDdbfirst;Trusted_Connection=True;");*/
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<EmployeeMaster>(entity =>
+            {
+                entity.HasKey(e => e.EmployeeId)
+                    .HasName("PK__Employee__7AD04FF15142BD21");
+
+                entity.ToTable("EmployeeMaster");
+
+                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+
+                entity.Property(e => e.EmployeeFirstName)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.EmployeeLastName)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
